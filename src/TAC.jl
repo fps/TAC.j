b46430917629078b56f17be6125167d95a1833f9
fps <mista.tapas@gmx.net>
2025-04-19 18:20:31 +0200

    Update TAC.jl

--- src/TAC.jl
+++ src/TAC.jl
@@ -2,20 +2,20 @@ module TAC
     const log_cond = Ref{Any}()
 
     function f()
-      	Threads.@spawn for n in 1:10
-    		sleep(1)
-    		ccall(:uv_async_send, Cint, (Ptr{Cvoid},), log_cond[])
-  	    end
-	end
+        Threads.@spawn for n in 1:10
+            sleep(1)
+            ccall(:uv_async_send, Cint, (Ptr{Cvoid},), log_cond[])
+        end
+    end
 
     export f
 
-	function __init__()
+    function __init__()
         @info "init"
         log_cond[] = Base.AsyncCondition() do cond 
-	        @debug "cond-debug"
+            @debug "cond-debug"
             @info "cond-info"
-	        println("cond-print")
+            println("cond-print")
         end
         f()
     end
