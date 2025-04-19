9cd83d67285f4e8a23b4124357b9b2e5788f520a
Florian Paul Schmidt <mista.tapas@gmx.net>
2025-04-19 15:53:10 +0000

    ok

--- src/TAC.jl
+++ src/TAC.jl
@@ -1,5 +1,18 @@
 module TAC
+    const log_cond = Ref{Any}()
 
-greet() = print("Hello World!")
+    function f()
+      	Threads.@spawn for n in 1:10
+    		sleep(1)
+    		ccall(:uv_async_send, Cint, (Ptr{Cvoid},), log_cond[])
+  	end
 
+    export f    function __init__()
+        @info "init"
+        log_cond[] = Base.AsyncCondition() do cond 
+	      @debug "cond"
+	      println("la")
+
+        f()
+    end
 end # module TAC
